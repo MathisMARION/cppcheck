34ea1830fd177501d8b749e7a9c53769fa055e89
Mathis Marion <mathis.marion@silabs.com>
2025-03-10 16:12:09 +0100

    fix scanf() confusion between uint8_t and string literal
    This incorrect warning would appear:
    
        void f(unsigned char *buf)
        {
            scanf("%hhu", buf + 1);
        }
    
        warning: %hhu in format string (no. 1) requires 'unsigned char *' but the argument type is 'const char *'. [invalidScanfArgType_int]
            scanf("%hhu", buf + 1);
            ^
    
    Note that the parent commit fixes #7038 in another way.
    
    Revert: ca4e5ac52 ("Fix #7038 false negative for string argument in the CheckIO (#3649)")

--- lib/checkio.cpp
+++ lib/checkio.cpp
@@ -1399,8 +1399,6 @@ CheckIO::ArgumentInfo::ArgumentInfo(const Token * arg, const Settings &settings,
             for (int p = 0; p < valuetype->pointer; p++)
                 tempToken->insertToken("*");
             tempToken = const_cast<Token*>(typeToken);
-            if (top->isBinaryOp() && valuetype->pointer == 1 && (valuetype->type == ValueType::CHAR || valuetype->type == ValueType::WCHAR_T))
-                tempToken->tokType(Token::eString);
             if (valuetype->isPrimitive())
                 tempToken->isStandardType(true);
             return;
